9477a98eef4b769831ef554dd275da443ad9a601
Maartje Verbeet <maartjeverbeet@gmail.com>
2017-06-19 15:13:24 +0200

    login via database

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -10,11 +10,9 @@ routes.get('/login', function(request, response){
     var email = credentials.name;
     var password = credentials.pass;
 
-
-    // Validate the input
     if (validator.isEmail(email) && !validator.isEmpty(password)) {
         // Load hash from your password DB.
-        db.query('SELECT * FROM customer WHERE email = ? AND password = ?', [email, password], function (error, results, fields) {
+        db.query('SELECT * FROM customer WHERE email = ?', [email], function (error, results, fields) {
             if (error) {
                 // Error handling for select db query
                 console.log(error);
@@ -24,21 +22,10 @@ routes.get('/login', function(request, response){
                 })
             } else {
                 if (results.length > 0) {
-                    // Compare db hash and body hash
-                    bcrypt.compare(password, results[0].hash, function (err, result) {
-                        if (result) {
-                            response.status(200);
-                            response.json({
-                                "token": auth.encodeToken(results[0].customer_id),
-                                "email": email
-                            });
-                        } else {
-                            // Hash comparing resulted in false
-                            response.status(401);
-                            response.json({
-                                "error": "Invalid credentials"
-                            });
-                        }
+                    var token = auth.encodeToken(username);
+                    response.status(200);
+                    response.json({
+                        "token": token,
                     });
                 } else {
                     response.status(401);
@@ -49,8 +36,8 @@ routes.get('/login', function(request, response){
             }
         })
     } else {
-        res.status(401);
-        res.json({
+        response.status(401);
+        response.json({
             'error': 'Invalid credentials'
         })
     }
