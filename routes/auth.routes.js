de61ce7ecaab92eb7a0b4ec597f5e76ad4edc9d5
Maartje Verbeet <maartjeverbeet@gmail.com>
2017-06-19 14:51:59 +0200

    inlog via database niet meer hard coded

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,24 +5,54 @@ var express = require('express');
 var routes = express.Router();
 var auth = require('../auth/authentication');
 
-routes.post('/login', function(req, res){
+routes.get('/login', function(request, response){
+    var credentials = basic_auth(request);
+    var email = credentials.name;
+    var password = credentials.pass;
 
-    console.dir(req.body);
 
-    var username = req.body.username;
-    var password = req.body.password;
-
-    if(username == "username" && password =="test") {
-        var token = auth.encodeToken(username);
-        res.contentType('application/json');
-        res.status(200);
-        res.json({token: token});
+    // Validate the input
+    if (validator.isEmail(email) && !validator.isEmpty(password)) {
+        // Load hash from your password DB.
+        db.query('SELECT * FROM customer WHERE email = ? AND password = ?', [email, password], function (error, results, fields) {
+            if (error) {
+                // Error handling for select db query
+                console.log(error);
+                response.status(500);
+                response.json({
+                    'error': 'Something went wrong when querying the database, check stdout'
+                })
+            } else {
+                if (results.length > 0) {
+                    // Compare db hash and body hash
+                    bcrypt.compare(password, results[0].hash, function (err, result) {
+                        if (result) {
+                            response.status(200);
+                            response.json({
+                                "token": auth.encodeToken(results[0].customer_id),
+                                "email": email
+                            });
+                        } else {
+                            // Hash comparing resulted in false
+                            response.status(401);
+                            response.json({
+                                "error": "Invalid credentials"
+                            });
+                        }
+                    });
+                } else {
+                    response.status(401);
+                    response.json({
+                        "error": "Invalid credentials"
+                    });
+                }
+            }
+        })
     } else {
-        res.contentType('application/json');
         res.status(401);
         res.json({
-            error: "ongeldige usernaam of password."
-        });
+            'error': 'Invalid credentials'
+        })
     }
 });
 
