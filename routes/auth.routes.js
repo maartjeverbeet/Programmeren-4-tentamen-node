419d322ee1bd134da869b72f5292bd7ba96f4d77
Maartje Verbeet <maartjeverbeet@gmail.com>
2017-06-19 15:39:39 +0200

    test

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,41 +6,32 @@ var routes = express.Router();
 var auth = require('../auth/authentication');
 
 routes.post('/login', function(request, response){
-    var credentials = basic_auth(request);
-    var email = credentials.name;
-    var password_user = credentials.pass;
+    var email = request.body.name;
+    var password_user = request.body.password_user;
 
-    if (validator.isEmail(email) && !validator.isEmpty(password_user)) {
-        // Load hash from your password DB.
-        db.query('SELECT * FROM customer WHERE email = ? AND password_user = ?', [email, password_user], function (error, results, fields) {
-            if (error) {
-                // Error handling for select db query
-                console.log(error);
-                response.status(500);
+    db.query('SELECT * FROM customer WHERE email = ? AND password_user = ?', [email, password_user], function (error, results, fields) {
+        if (error) {
+            // Error handling for select db query
+            console.log(error);
+            response.status(500);
+            response.json({
+                'error': 'Something went wrong when querying the database, check stdout'
+            })
+        } else {
+            if (results.length > 0) {
+                var token = auth.encodeToken(username);
+                response.status(200);
                 response.json({
-                    'error': 'Something went wrong when querying the database, check stdout'
-                })
+                    "token": token,
+                });
             } else {
-                if (results.length > 0) {
-                    var token = auth.encodeToken(username);
-                    response.status(200);
-                    response.json({
-                        "token": token,
-                    });
-                } else {
-                    response.status(401);
-                    response.json({
-                        "error": "Invalid credentials"
-                    });
-                }
+                response.status(401);
+                response.json({
+                    "error": "Invalid credentials"
+                });
             }
-        })
-    } else {
-        response.status(401);
-        response.json({
-            'error': 'Invalid credentials'
-        })
-    }
+        }
+    })
 });
 
 module.exports = routes;
